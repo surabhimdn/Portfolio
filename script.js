38fad33b6df3cf54b45657d8ac489528e07d2740
surabhimdn <165384167+surabhimdn@users.noreply.github.com>
2024-06-20 00:24:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function bookOpen(){
+    window.open("index.html");
+}
+
+function newTab(x){
+    if (x==1) window.open("3 About me.html");
+    else if (x==2) window.open("4 Projects.html");
+    else window.open("5 Extra curricular.html");
+}
